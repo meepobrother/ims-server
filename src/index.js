b337b45d7960370972aefe60820b6061dd2f269e
imeepos <1037483576@qq.com>
2019-04-03 09:07:36 +0800

    proxy2

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
-const express = require('express')
-const app = express();
-
-app.get('/', (req, res, next) => {
-    res.end(`hello world`)
-})
-
-app.listen(3000, () => {
-    console.log(`app bootstrap`)
-});
+shelljs.exec(`proxy2 server -p 80`);
